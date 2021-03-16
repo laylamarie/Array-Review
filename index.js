646a4c923ef76c31c0b3bac38accad61561f6a03
Jonathan Corley <jcorley@noscihigh.org>
2021-03-16 16:07:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+//Declare and initalize a variable with an empty array 
+
+
+//Use bracket notation to add
+  //a string to the first index
+  //a boolean to the second index
+  //a number to the fourth index
+
+
+//When you console.log the third and fourth indices 
+//what is the value?
+
+
+//What is the length of the array?
+
+
+//Use the push method to insert a string of your favorite tv show.
+
+
+//Use the unshift method to add three values
+
+
+//console.log the array
+
+
+//
+
+
+
+
+//Practice problems
+
+/*
+1. Write a function that will return the number 
+   after the input number.
+
+Example:
+nextNum(4) => 5
+nextNum(100) => 101
+
+*/
+
+function nextNum(number){
+  //CODE BELOW HERE
+
+
+
+  //CODE ABOVE HERE
+}
+
+
+
+
+
+/*
+2. Write a function that can count the number of 
+   vowels in a string.
+
+EXAMPLE:
+  countVowels('hello') => 2
+  countVowels('stop') => 1
+
+*/
+
+function countVowels(string){
+  //CODE BELOW HERE
+
+
+  //CODE ABOVE HERE
+}
+
+/*
+3. Write a function that will return if a given 
+   array has an even number of indicies.
+
+EXAMPLE:
+var myArray = [0,1,2,3]
+var otherArray = [0,1,2,3,4]
+
+evenIndicies(myArray) => true
+evenIndicies(otherArray) => false
+*/
+
+
+function evenIndicies(array){
+  //CODE BELOW HERE
+
+
+  //CODE ABOVE HERE
+}
